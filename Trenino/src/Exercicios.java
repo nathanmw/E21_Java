9a4fd6acf36070b6f3ba6f0d26ce20bf9c83e811
Nathan <nathanwachholz@hotmail.com>
2020-09-27 20:19:09 -0300

    Exercicios 01 IF ELSE

---
+++ Trenino/src/Exercicios.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+		Scanner entradaDados = new Scanner(System.in);
+		double valor1, valor2;
+
+		try {
+			System.out.println("Digito o Primeiro Valor:");
+			valor1 = entradaDados.nextDouble();
+			System.out.println("Digite o Segundo Valor:");
+			valor2 = entradaDados.nextDouble();
+
+			if (valor1 >= valor2) {
+				System.out.println("O Maior valor é o primeiro:[" + valor1 + "]");
+			} else {
+				System.out.println("O Maior valor é o segundo:[" + valor2 + "]");
+
+			}
+		} catch (Exception e) {
+			System.out.println("Caracter INVALIDO, Digite apenas nuneros!");
+		}
+
+		entradaDados.close();
+	}
+
+}
